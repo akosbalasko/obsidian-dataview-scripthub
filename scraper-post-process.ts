ab217e17850c304ff1b604988b1ccd445a64cc68
Akos Balasko <akos0215@gmail.com>
2021-08-30 23:17:49 +0200

    action and postscript updated

---
+++ scraper-post-process.ts
@@ -0,0 +1,43 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
+import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+
+interface Post {
+    creator?: {
+        name: string,
+        url: string,
+    }
+    datePublished?: Date,
+    content?: string,
+}
+
+// The filename is the first invocation argument
+const filename = Deno.args[0] // Same name as downloaded_filename
+//const data = await readJSON(filename)
+const html = await Deno.readTextFile("./dataview-plugin-snippet-showcase.html");
+const $ = cheerio.load(html);
+
+const posts = $('div[class="topic-body crawler-post"]');
+var rawData: Array<Post> = []
+await posts.each(function(index, data) {
+    const post = cheerio.load(data);
+    const creatorName = post('a[itemprop=url] > span[itemprop=name]');
+    const creatorUrl = post('span[class=creator] > a[itemprop=url]');
+    const publishedDate = post("span[class=crawler-post-infos] > time[itemprop=datePublished]");
+    const publishDate = publishedDate.first().attr('datetime')?.toString();
+    const content = post('div[class=post]')?.html() || '';
+    rawData.push({
+        creator: {
+            url: creatorUrl.first().attr('href')?.toString() || '',
+            name: creatorName.text(),
+        },
+        datePublished: publishDate ? new Date(publishDate): new Date(),
+        content,
+    })
+    }
+);
+const dataviewCodes = rawData.filter(post => post.content?.includes('```dataview'));
+for (const codePosts of dataviewCodes){
+    const fileName =`${codePosts.creator?.name || ''}_${codePosts.datePublished?.getTime()}.json` 
+    await writeJSON(`./scripts/${fileName}`, codePosts);
+}
+console.log(JSON.stringify(dataviewCodes));// Pluck a specific key off
